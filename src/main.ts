39044d9779ddcc6d6bde0eceaf48999e513b8d87
Oded Lazar <odedl@talon-sec.com>
2022-02-22 13:51:31 +0200

    Add more logs and remove body prefix

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import fetch from 'node-fetch';
@@ -8,7 +9,6 @@ async function run() {
       githubToken = core.getInput('github-token', { required: true }),
       atlassianToken = core.getInput('atlassian-token', { required: true }),
       atlassianDomain = core.getInput('atlassian-domain', { required: true }),
-      ticketBodyPrefix = core.getInput('ticket-body-prefix', { required: true }),
       titleRegex = new RegExp(core.getInput('ticket-regex', { required: true }),
         core.getInput('title-regex-flags') || 'g'),
       title = github.context!.payload!.pull_request!.title,
@@ -17,16 +17,20 @@ async function run() {
     core.info(`Checking "${titleRegex.source}" with "${titleRegex.flags}" flags against the PR title: "${title}"`);
     let match = titleRegex.exec(title)
     if (!match) {
-      core.setFailed("The PR title/body is missing a JIRA ticket");
+      core.setFailed("The PR title is missing a JIRA ticket");
       return;
     }
+ 
+    core.info(`The PR title matches!`);
 
     if (!match.groups || !match.groups['ticket']) {
-      core.setFailed("The ticket key is missing from the title-regex");
+      core.setFailed("The ticket key is missing from the ticket-regex");
       return;
     }
 
     const ticket = match.groups['ticket'];
+    core.info(`Verifying that ticket ${ticket} exists in JIRA`);
+
     const response = await fetch(`https://${atlassianDomain}/rest/api/3/issue/${ticket}`, {
       method: 'GET',
       headers: {
@@ -40,26 +44,34 @@ async function run() {
       core.setFailed(`Unknown JIRA ticket: ${ticket}: ${data}`);
       return;
     }
+    core.info(`JIRA ticket ${ticket} found`);
 
     const client: github.GitHub = new github.GitHub(githubToken);
     const pr = github.context.issue;
 
-    const bodyTicket = `${ticketBodyPrefix} ${ticket}`;
-    if (!body.includes(bodyTicket)) {
-      core.warning(`body doesn't include: ${bodyTicket}`);
+    const ticketRef = `[${ticket}](https://${atlassianDomain}/browse/${ticket})`;
+    if (body.includes(ticketRef)) {
+      core.debug(`PR body already contains ${ticket} url`);
+      return;
+    }
+    
+
+    core.info(`JIRA ticket ${ticket} exists in PR body, replacing with URL`);
+    const newBody = body.replace(ticket, ticketRef);
+    if (newBody == body) {
+      // body hasn't changed, no need to issue an update
       return;
     }
 
-    const bodyTicketWithUrl = `${ticketBodyPrefix} [${ticket}](https://${atlassianDomain}/browse/${ticket})`;
-    const newBody = body.replace(bodyTicket, bodyTicketWithUrl);
     client.pulls.update({
       owner: pr.owner,
       repo: pr.repo,
       pull_number: pr.number,
       body: newBody
     });
-
+    core.info("done!");
   } catch (error) {
+    core.error(error.message);
     core.setFailed(error.message);
   }
 }
