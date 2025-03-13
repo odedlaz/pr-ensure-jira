fc06c3378b6c17ab2cd6fdbf146bb1ab8e6a41af
Oded Lazar <2660175+odedlaz@users.noreply.github.com>
2025-03-13 11:11:08 +0200

    internal error

--- src/main.ts
+++ src/main.ts
@@ -84,6 +84,7 @@ async function runAction() {
   return run(atlassianToken, atlassianDomain, titleRegex, branchNameRegex);
   } catch (error) {
     core.error(`internal error: ${JSON.stringify(error)}`);
+    core.setOutput('error', 'internal-error')
   }
 }
 
